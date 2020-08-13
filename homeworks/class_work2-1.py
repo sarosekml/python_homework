ceb3c7f464db76a87e9fc12060e0438468316323
Mikhail Sarosek <sarosekml@gmail.com>
2020-08-13 21:48:11 +0700

    add changes

---
+++ homeworks/class_work2-1.py
@@ -0,0 +1,24 @@
+my_str = 'hello my friends'
+
+for char in my_str:
+   print(char)
+
+#for char in reversed(my_str):
+#    print(char)
+
+# len_obj = len(my_str)
+#
+# while len_obj:
+#     len_obj -= 1
+#     char = my_str[len_obj]
+#     print(char)
+
+# iter_obj = my_str.__iter__()
+#
+# while True:
+#     try:
+#         char = next(iter_obj)
+#     except StopIteration:
+#         break
+#     print(char)
+
---
+++ homeworks/class_work2-2.py
@@ -0,0 +1,24 @@
+
+my_list = [1, 2, 3, 'hello', True, False, [4, 5, 6]]  # список list(), зменяемы тип данных
+
+my_tuple = (1, 2, 3, 'hello', True, False, [4, 5, 6])  # кортеж tuple(), неизменяемый тип данных
+
+my_matrix = [[1,2,3], [4,5,6], [7,8,9]]
+
+my_set = {1, 2, 3, 4, 5, 6, 'hello'}
+
+my_dict = {'key': 'hello', 1: [1, 2, 3, 4], 22.3: {1: 22}, True: (1, 2, 3)}  # словарь dict() изменяемы тип данных
+
+# for idx, itm in enumerate(my_list, 1):
+#     print(idx, '-', itm)
+
+
+# for key, value in my_dict.items():
+#     print(key, '-', value)
+
+# for idx, value in enumerate(my_dict.values()):
+#     print(value, idx)
+
+
+for value in enumerate(my_set):
+    print(value)
